f5b7cc8ef3c9eb485fd3b14be17bd17e987631df
aniket4518 <jhaaniket2005@gmail.com>
2026-01-24 08:56:21 +0530

    removed dist

--- dist/middelware/authmiddleware.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Socket } from "socket.io";
-export default function socketAuth(socket: Socket, next: (err?: Error) => void): Promise<void>;
-//# sourceMappingURL=authmiddleware.d.ts.map
